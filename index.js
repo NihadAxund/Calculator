065707df3e0d596299cb1a73f4bc7f39517d396f
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2023-03-09 02:26:55 +0400

    Son

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let element = document.getElementById("MathID");
 function Call_d1(id) {
     let item = document.getElementById(id);
-    if(element.innerHTML.length>26) {
+    if (element.innerHTML.length > 26) {
         return;
     }
-    else if (element.innerHTML == "0"||element.innerHTML=="Infinity"||element.innerHTML=="-Infinity") {
+    else if (element.innerHTML == "0" || element.innerHTML == "Infinity" || element.innerHTML == "-Infinity") {
         element.innerHTML = "";
     }
     element.innerHTML += item.id;
@@ -24,3 +24,18 @@ function Call_d2(id) {
 function Ac_Btn() {
     element.innerHTML = "0";
 }
+
+const checkbox = document.getElementById('checkbox');
+const root = document.documentElement;
+checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+        document.body.classList.toggle('dark');
+        root.style.setProperty('--Color3', 'white');
+        root.style.setProperty('--Color2','rgb(31, 31, 31)')
+
+       // alert('Checkbox seçildi!');
+    } else {
+        root.style.setProperty('--Color3', 'rgb(31, 31, 31)');
+        root.style.setProperty('--Color2','white')
+    }
+})
